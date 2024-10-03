ff4ff23c9cf23dca4bc2b85ddcaf083dcf9a25a3
idclla <114296917+idclla@users.noreply.github.com>
2024-10-03 23:28:22 +0800

    modal

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-import React from 'react'
-import Login from './components/Login'
-import Registration from './components/Registration'
-import LandingPage from './components/LandingPage'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
+import Registration from './components/Registration';
+import LandingPage from './components/LandingPage';
 
 function App() {
   return (
-    <div>
-      <Login />
-      <Registration />
-      <LandingPage />
-    </div>
-  )
+    <Router>
+      {/* <nav>
+        <ul>
+          <li>
+            <Link to="/">Landing Page</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/registration">Registration</Link>
+          </li>
+        </ul>
+      </nav> */}
+
+      <Routes>
+        <Route path="/login" element={<Login />} /> {/* Change made here */}
+        <Route path="/registration" element={<Registration />} />
+        <Route path="/landingpage" element={<LandingPage />} />
+        <Route path="*" element={<h2>404 Not Found</h2>} /> {/* Fallback for unmatched routes */}
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
